e7c908731294d93d452c09529005573ad10f1cbb
ouyangyuro <def2446@gmail.com>
2023-03-27 20:42:07 +0800

    feat: basic settings tailwind, light theme and dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // using `src` directory:
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
