74e2f1a6666c2e2baaf2e4c8bfaa866eb4ba0679
ouyangyuro <def2446@gmail.com>
2023-04-15 17:52:25 +0800

    feat: export .env and give example

---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+/**
+ * --------------------------------------------------------------------------------------------------------------
+ * TDX
+ */
+export const TDXKey = process.env.NEXT_PUBLIC_TDX_apiKey;
+export const TDXId = process.env.NEXT_PUBLIC_TDX_apiId;
+export const API_HOSTNAME_URL = process.env.NEXT_PUBLIC_API_SERVER_HOSTNAME;
