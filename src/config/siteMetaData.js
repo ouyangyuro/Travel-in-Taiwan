6dbc29e440f4a0844aa0e94700eaf55f6929b686
ouyangyuro <def2446@gmail.com>
2023-04-16 17:47:00 +0800

    feat: add head's infomation for SEO

---
+++ src/config/siteMetaData.js
@@ -0,0 +1,19 @@
+import {
+    GOOGLE_SITE_VERIFICATION,
+    GOOGLE_CLIENT_ID,
+    FB_APP_ID,
+  } from 'src/config/env';
+
+const siteMetaData = {
+    title: 'Travel In Taiwan',
+    keywords: '台灣、旅遊、找去處、Travel、Taiwan',
+    description:
+      'Travel In Taiwan 提供所有台灣旅遊好去處的吃、喝、玩、樂活動資訊。',
+    google_site_verification: `${GOOGLE_SITE_VERIFICATION}`,
+    fb_app_id: `${FB_APP_ID}`,
+    google_client_id: `${GOOGLE_CLIENT_ID}`,
+    image: '/images/icon.png',
+    iss: 'vercel.app',
+  };
+
+  export default siteMetaData;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
+import Head from 'next/head';
 import { appWithTranslation } from 'next-i18next';
 import { Provider } from 'react-redux';
 import { store } from 'src/redux/store';
+
+import siteMetaData from 'src/config/siteMetaData';
 
 import 'src/assets/styles/globals.scss';
 
@@ -9,7 +12,31 @@ function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+    <Provider store={store}>
+      {getLayout(
+        <>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+            />
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+            {/* TODO: Google */}
+            {/* <meta
+              name="google-site-verification"
+              content={siteMetaData.google_site_verification}
+            /> */}
+
+            {/* TODO: FB */}
+            {/* <meta property="fb:app_id" content={siteMetaData.fb_app_id} />
+            <meta property="og:site_name" content={siteMetaData.title} /> */}
+          </Head>
+          <Component {...pageProps} />
+        </>
+      )}
+    </Provider>
   );
 }
 
