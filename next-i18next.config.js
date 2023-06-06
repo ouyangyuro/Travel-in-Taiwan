844350a02d9c7e17a6d358ff0a0573c0f13af286
ouyangyuro <def2446@gmail.com>
2023-06-06 20:07:15 +0800

    fix: Set localePath to fix serverSideTranslations crashes when called inside getServerSideProps on Vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,14 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'zhHant',
     locales: ['en', 'zhHant'],
     // Set LocalDetection to false so that you not get redirected to "zhHant" as default
     localeDetection: false,
-    // Set localePath to fix serverSideTranslations crashes when called inside getServerSideProps on Vercel
-    localePath: path.resolve('./public/locales')
   },
+      // Set localePath to fix serverSideTranslations crashes when called inside getServerSideProps on Vercel
+  ...(typeof window === undefined
+    ? { localePath: path.resolve('./public/locales') }
+    : {}),
 }
